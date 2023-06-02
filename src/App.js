7d7d51367be332802d43c93ae808220d0d10108e
mahmoud-wahba-dev <mahmoud.wahba.dev@gmail.com>
2023-06-03 01:26:45 +0300

    lesson 33 mapping on monsters array and add key attribute

--- src/App.js
+++ src/App.js
@@ -4,35 +4,27 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-
-  constructor (){
+  constructor() {
     super();
     this.state = {
-      name : {firstName: "yihua" , lastName : "zhang"},
-      company: "ztm"
-    }
+      monsters: [{ name: "linda" , id:"hdb55s" },
+                  { name: "Frank" , id: "asds54" },
+                  { name: "jackyu", id: "ads54" },
+                {name: "andrie" , id: "ss884"},
+                {name: "andrie" , id: "ssa455sad1" }
+                ],
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-        hi  { this.state.name.firstName  } {this.state.name.lastName  } , i work at {this.state.company}
-          </p>
-        
-          <button onClick={()=>{
-              this.setState(()=>{  
-                return {name: {firstName: "andrie", lastName:"neagoie"}} 
-                }   , ()=> {
-                  console.log(this.state);
-                }  )
-              // console.log(this.state);
-          }}>
-            change name
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster,index)=>{
+            console.log(index);
+         return   <h1 key={monster.id }> {monster.name} </h1>
+          })
+        }
       </div>
     );
   }
